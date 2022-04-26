91e5a8d94ad1400b90cd83454f5e56e13a43041e
raxracks <multisniperism@gmail.com>
2022-04-26 23:18:55 +1200

    clean up structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SongsController } from './api/v1/songs/songs.controller';
-import { SongsService } from './api/v1/songs/songs.service';
-import { ArtistsService } from './api/v1/artists/artists.service';
-import { ArtistsController } from './api/v1/artists/artists.controller';
+import { SongsController } from './v1/songs/songs.controller';
+import { SongsService } from './v1/songs/songs.service';
+import { ArtistsService } from './v1/artists/artists.service';
+import { ArtistsController } from './v1/artists/artists.controller';
 
 @Module({
   imports: [],
